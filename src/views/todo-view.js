c078c4698c1ee8488983d50b152de0b43d27fefc
@av.moskalenko <av.moskalenko@csltd.com.ua>
2019-10-08 18:55:44 +0300

    jquery picker lit-element

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,5 +1,6 @@
 import { LitElement, html } from 'lit-element';
-
+import * as JQuery from 'jquery/dist/jquery.min';
+import * as picker from 'jquery-datetimepicker/build/jquery.datetimepicker.full.min';
 class TodoView extends LitElement {
   render() {
     return html`
